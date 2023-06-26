fc13adf9ebc1e25013822bbdd36afaac1c82fe7f
Trevor McCourt <trevormccourt@Trevors-MacBook-Pro.local>
2023-06-26 03:06:22 -0400

    scaled gaussian sample

--- sampled_fft/sampled_fft.py
+++ sampled_fft/sampled_fft.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 
-def sample_scaled_shifted_gaussians(sigmas, mus, standard_gauss_sampler):
-    n_samples = len(sigmas) * len(mus)
-    samples = standard_gauss_sampler(n_samples).reshape((len(sigmas), len(mus), -1))
-    mus = mus.
+def sample_scaled_shifted_gaussians(n_samples_per, sigmas, mus, standard_gauss_sampler):
+    n_sigmas = len(sigmas)
+    n_mus = len(mus)
+    samples = standard_gauss_sampler(n_sigmas * n_mus * n_samples_per).reshape(
+        (-1, n_sigmas, n_mus))
+    return np.einsum("bda, d -> bda", samples + mus, sigmas)
